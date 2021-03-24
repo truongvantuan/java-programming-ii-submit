bd134726924ee67dec81e48609ff8f543c3c0544
truongvantuan <truongvantuan@outlook.com.vn>
2021-03-24 21:23:06 +0700

    submit part08_06

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 public class Nicknames {
 
     public static void main(String[] args) {
-        // Do the operations required here!
+        HashMap<String, String> nickNames = new HashMap<>();
+
+        nickNames.put("matthew", "matt");
+        nickNames.put("michael", "mix");
+        nickNames.put("arthu", "artie");
+        System.out.println(nickNames.get("matthew"));
     }
 
 }
