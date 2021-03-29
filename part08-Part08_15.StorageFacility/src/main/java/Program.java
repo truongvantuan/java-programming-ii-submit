81cc70f00b522944a060d937900a220ea268c968
truongvantuan <truongvantuan@outlook.com.vn>
2021-03-29 10:14:23 +0700

    submit part08_15 | finish part08

--- part08-Part08_15.StorageFacility/src/main/java/Program.java
+++ part08-Part08_15.StorageFacility/src/main/java/Program.java
@@ -2,6 +2,21 @@
 public class Program {
 
     public static void main(String[] args) {
+
+        StorageFacility facility = new StorageFacility();
+        facility.add("a14", "ice skates");
+        facility.add("a14", "ice hockey stick");
+        facility.add("a14", "ice skates");
+
+        facility.add("f156", "rollerblades");
+        facility.add("f156", "rollerblades");
+
+        facility.add("g63", "six");
+        facility.add("g63", "pi");
+
+        System.out.println(facility.contents("a14"));
+        System.out.println(facility.contents("f156"));
+
         // Test your program here
 //        StorageFacility facility = new StorageFacility();
 //        facility.add("a14", "ice skates");
@@ -18,8 +33,5 @@ public class Program {
 //
 //        System.out.println(facility.contents("f156"));
 //        System.out.println(facility.storageUnits());
-
-          
-
     }
 }
