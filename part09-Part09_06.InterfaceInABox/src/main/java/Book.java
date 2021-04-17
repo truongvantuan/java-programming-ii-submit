ed59d47bac48864f1bcc5c3dde156d067d9baf50
truong tuan <truongvantuan@outlook.com.vn>
2021-04-17 16:51:11 +0700

    submit part09_06

---
+++ part09-Part09_06.InterfaceInABox/src/main/java/Book.java
@@ -0,0 +1,23 @@
+
+public class Book implements Packable {
+
+    private final String author;
+    private final String name;
+    private final double weight;
+
+    public Book(String author, String name, double weight) {
+        this.author = author;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public double weight() {
+        return this.weight;
+    }
+    
+    @Override
+    public String toString() {
+        return this.author + ": " + this.name;
+    }
+}
