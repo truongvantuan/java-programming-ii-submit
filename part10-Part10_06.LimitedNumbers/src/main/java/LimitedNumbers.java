9768cdd2c768dfb924a5a367909ec674d94a0c07
Trương Tuấn <truongvantuan@outlook.com.vn>
2021-05-11 09:18:18 +0700

    submit part10_05 -> 07

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,23 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Integer> inputs = new ArrayList<>();
 
+        while (true) {
+            int value = Integer.valueOf(scanner.nextLine());
+            if (value < 0) {
+                break;
+            }
+
+            inputs.add(value);
+        }
+
+        inputs.stream().filter(number -> number >= 1 && number <= 5).forEach(s -> System.out.println(s));
     }
 }
