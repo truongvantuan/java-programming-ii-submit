bdb9be397d93b2e4307d272e7a2732ea182335ef
truongtuan <truongvantuan@outlook.com.vn>
2021-05-22 15:48:08 +0700

    submit part10_12

--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
@@ -1,10 +1,9 @@
 
-
 public class MainProgram {
 
     public static void main(String[] args) {
-//        Student first = new Student("jamo");
-//        Student second = new Student("jamo1");
-//        System.out.println(first.compareTo(second));
+        Student first = new Student("jamo");
+        Student second = new Student("jamo1");
+        System.out.println(first.compareTo(second));
     }
 }
--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
@@ -1,8 +1,7 @@
 
+public class Student implements Comparable<Student> {
 
-public class Student {
-
-    private String name;
+    private final String name;
 
     public Student(String name) {
         this.name = name;
@@ -15,6 +14,11 @@ public class Student {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.name.compareTo(student.getName());
     }
 
 }
